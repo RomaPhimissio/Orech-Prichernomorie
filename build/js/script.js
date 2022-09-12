037c33af527446a72ff68985e1fb1fba949002da
Your Name <you@example.com>
2022-09-12 21:30:40 +0300

    add section Producer

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,4 @@
+// BurgerMenu
 const burgerMenu = document.querySelector('.header__burger-wrap');
 if (burgerMenu) {
     const headerNav = document.querySelector('.header__nav');
@@ -9,21 +10,49 @@ if (burgerMenu) {
     })
 }
 
-const videoPlay = document.querySelector('.showing__link');
+// VideoShowing
+const videoPlayShow = document.querySelector('.showing__link-play');
 const videoMovie = document.querySelector('.showing__video');
 
-if (videoPlay) {
+
+if (videoPlayShow) {
     const showIng = document.querySelector('.showing');
-    videoPlay.addEventListener("click", function (e) {
+    videoPlayShow.addEventListener('click', function (e) {
         showIng.classList.add('active');
         videoMovie.play();
-        videoMovie.addEventListener("ended", function () {
+        videoMovie.addEventListener('ended', function () {
             videoMovie.load()
             showIng.classList.remove('active');
         })
 
     })
-
-
 }
 
+// VideoProducer
+const videoPlayProd = document.querySelector('.producer__link-play');
+const videoMovieProd = document.querySelector('.producer__video');
+
+if (videoPlayProd) {
+    const produCe = document.querySelector('.producer');
+    videoPlayProd.addEventListener("click", function (e) {
+        produCe.classList.add('active');
+        videoMovieProd.play();
+        videoMovieProd.addEventListener("ended", function () {
+            videoMovieProd.load()
+            produCe.classList.remove('active');
+        })
+    })
+}
+
+// Slider
+// const slider = document.querySelector('.producer__slider');
+// let count = 0;
+// let width;
+
+// function init() {
+//     console.log('resize');
+//     width = document.querySelector('.producer__slider-block').offsetWidth;
+//     console.log(width);
+// }
+
+// init()
