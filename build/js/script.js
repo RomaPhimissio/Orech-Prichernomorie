a57d9ed8aea6c09f6c343a7e9e599a39927f18a9
Your Name <you@example.com>
2022-11-08 23:36:16 +0200

    last change

--- build/js/script.js
+++ build/js/script.js
@@ -260,3 +260,43 @@ new Swiper('.producer__slider', {
     // ширина слайда
     slidesPerView: 'auto'
 })
+
+
+// ! Lazy Loading
+
+// const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]');
+// const windowHeight = document.documentElement.clientHeight;
+
+// let lazyImagesPositions = [];
+// if (lazyImages.length > 0) {
+//     lazyImages.forEach(img => {
+//         if (img.dataset.src || img.dataset.srcset) {
+//             lazyImagesPositions.push(img.getBoundingClientRect().top + window.pageYOffset);
+//             lazyScrollCheck();
+//         }
+//     });
+// }
+
+// window.addEventListener("scroll", lazyScroll);
+
+// function lazyScroll() {
+//     if (document.querySelectorAll('img[data-src],source[data-srcset]').length > 0) {
+//         lazyScrollCheck();
+//     }
+// }
+
+// function lazyScrollCheck() {
+//     let imgIndex = lazyImagesPositions.findIndex(
+//         item => window.pageYOffset > item - windowHeight
+//     );
+//     if (imgIndex >= 0) {
+//         if (lazyImages[imgIndex].dataset.src) {
+//             lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+//             lazyImages[imgIndex].removeAttribute('data-src');
+//         } else if (lazyImages[imgIndex].dataset.srcset) {
+//             lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+//             lazyImages[imgIndex].removeAttribute('data-srcset');
+//         }
+//         delete lazyImagesPositions[imgIndex];
+//     }
+// }
